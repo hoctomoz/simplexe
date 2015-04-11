223b6b8eec40724e35a4d544e23b86ca1b1769be
pferme <pferme@ens-lyon.fr>
2015-04-11 16:06:13 +0200

    Print functions in simplex, allowed only if normalized expressions

--- linear.ml
+++ linear.ml
@@ -1,44 +1,36 @@
-let printArray a = Array.iter (fun x -> print_float x; print_char ' ') a;;
-
-let printMatrix m = Array.iter (fun a -> printArray a; print_newline()) m;;
-
 let ( *% ) lambda a = Array.map (fun x -> lambda *. x) a;;
 
-let normalize i a = (1. /. a.(i)) *% a;;
+let normalize i a = (- 1. /. a.(i)) *% a;;
 
 let substitute i a b =
 (* Substitue la variable i de a par l'équation donnée dans b *)
-  if b.(i) <> 1. then failwith "Erreur dans substitute : équation non normalisée";
+  if b.(i) <> -1. then failwith "Erreur dans substitute : équation non normalisée";
   
   let coeff = a.(i) in
-  Array.iteri (fun k -> fun x -> a.(k) <- x -. coeff*.b.(k)) a;;
+  Array.iteri (fun k -> fun x -> a.(k) <- x +. coeff*.b.(k)) a;;
 
 let substituteMatrix i j m =
 (* Substitue la variable j de m.(k) par l'équation donnée dans m.(i), pour k <> i *) 
   Array.iteri (fun k -> fun a -> substitute j a m.(i));;
 
-let enteringVariable const a =
+let enteringVariable a =
 (* Renvoie le plus petit indice i tel que a.(i) > 0. *)
-(* const correspond à la valeur contraignant (la constante), qui est stockée en position 0 dans notre modèle (on aura const = 0) *)
   let n = Array.length a in
-  let k = ref 0 in
+  let k = ref 1 in
   let check = ref true in
 
   while (!k < n && !check) do
-    if !k <> const then begin
-      if a.(!k) > 0.
-      then check := false
-      else incr k
-    end;
+    if a.(!k) > 0.
+    then check := false
+    else incr k
   done;
     
   if !check then None
   else Some !k
 ;;
 
-let leavingVariable i const m =
+let leavingVariable i m =
 (* Renvoie l'indice de la ligne de m où l'équation est la plus contraignante pour i *)
-(* const correspond à la valeur contraignant (la constante), qui est stockée en position 0 dans notre modèle (on aura const = 0) *)
   let n = Array.length m in
   let min = ref 0. in
   let indice = ref None in
@@ -46,7 +38,7 @@ let leavingVariable i const m =
   for k = 0 to n-1 do
     let a = m.(k) in
     if a.(i) < 0. then begin
-      let coeff = -. a.(const) /. a.(i) in
+      let coeff = -. a.(0) /. a.(i) in
       if coeff > !min then (min := coeff; indice := Some k);
     end;
   done;
--- simplex.ml
+++ simplex.ml
@@ -12,10 +12,39 @@ object (this)
   val constraints : value array array = Array.make_matrix nconsP (nvarP + nconsP + 2) 0.
   (* NB : On garde la constante en première position (matrix[i][0]) *)
   (* On rajoute une dernière colonne à la fin pour l'éventuelle première phase, sinon il faudrait redéfinir tout l'objet... *)
-  val variables : int array = Array.init nconsP (fun x -> x + nvarP + 1)
+  val variables : int array = Array.init nconsP (fun x -> x + nvarP)
   (* On repère quelle est la variable de chaque ligne que l'on assigne *)
 
   (* Méthodes *)
-  method print () = ()
+  method print_constraint i =
+    if constraints.(i).(variables.(i)) <> -1. then failwith "Erreur dans print_constraint : équation non normalisée";
+    print_string "x"; print_int variables.(i); print_string " = ";
+    print_float constraints.(i).(0);
+    
+    for k = 1 to nvar + ncons do
+      if k <> variables.(i) then begin
+	print_string " + ";
+	print_float constraints.(i).(k);
+	print_string "*x";
+	print_int k;
+      end;
+    done;
 
+    print_newline()
+
+  method print () =
+    for i = 0 to ncons-1 do
+      this#print_constraint i;
+    done;
+    for i = 0 to nvar + ncons + 1 do
+      print_string "-------";
+    done;
+    print_newline();
+    print_string "z = ";
+    print_float (objective.(0));
+    for i = 1 to nvar + ncons do
+      print_string " + ";
+      print_float (objective.(i));
+      print_string "*x"; print_int i;
+    done;
 end;;
